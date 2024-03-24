3e8495cb842bf448692a9bad64a279900edbd91b
Lahir-Sai-Vignesh <lahirsaivignesh3@gmail.com>
2024-03-24 19:11:22 +0530

    added components

---
+++ app/components/Sports.tsx
@@ -0,0 +1,167 @@
+// import React from 'react'
+
+// function Sports() {
+//   return (
+//     <div>
+//         <p className=' w-20 h-10 text-black font-bold underline'>Sports</p>
+        
+//     </div>
+//   )
+// }
+
+// export default Sports
+
+export default function Sports() {
+  return (
+    <div className="flex flex-col w-[1249px] box-sizing-border">
+      <div className="m-[0_0_23px_0] flex flex-col items-center self-start w-[fit-content] box-sizing-border">
+        <div className="m-[0_0.8px_5px_0] inline-block break-words font-['Poppins'] font-bold text-[24px] text-[#000000]">
+          Sports
+        </div>
+        <div className="bg-[#738FFF] w-[80.5px] h-[2px]">
+        </div>
+      </div>
+      <div className="m-[0_0_50px_0] flex flex-row gap-[0_14.1px] w-[fit-content] box-sizing-border">
+        <div className="shadow-[0px_4px_8px_0px_rgba(0,0,0,0.05)] bg-[#FFFFFF] relative flex flex-col p-[10px_10.1px_10px_10.1px] box-sizing-border">
+          <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/87ca0cee36ac5cfd911cbaf11e283d81103935c5?Expires=1711888365&KeyName=fn12-cdn&Signature=SSZPbGLHXd_Kwo9yiM0gI_1fCx8=')_50%_/_cover_no-repeat] relative m-[0_0_14.7px_0] w-[218.4px] h-[385.3px]">
+            <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/eefbbad1c707d3938e14e33be6c2e03f06a5b9ec?Expires=1711888365&KeyName=fn12-cdn&Signature=LpvyOMJ_5lPRF5RA1dMPNBsOwt4=')_50%_/_cover_no-repeat] absolute top-[0px] right-[-84.5px] w-[387.5px] h-[385px]">
+            </div>
+            <div className="absolute right-[-682.4px] bottom-[-74.7px] flex flex-row p-[0_0_67px_72.5px] w-[1089.5px] h-[523px] box-sizing-border">
+              <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/bd7492d712397c14a504f04de51a02dfe96c1722?Expires=1711888365&KeyName=fn12-cdn&Signature=e_PEY6U17LRPzeTvXN5ydmHzuaY=')_50%_/_cover_no-repeat] relative m-[0_160.5px_8px_0] w-[450.9px] h-[448px]">
+              </div>
+              <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/758cb06a6eda04d045a7a66a22fe27668df0cb39?Expires=1711888365&KeyName=fn12-cdn&Signature=VvY0IkiOQ7ynaMvjOKq0BpvdjvE=')_50%_/_cover_no-repeat] m-[53px_0_0_0] w-[405.6px] h-[403px]">
+              </div>
+              <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/d152d472212d9f2d7e308b20b2eba0e81845c4e2?Expires=1711888365&KeyName=fn12-cdn&Signature=NdMjIIyBj664cvgPR5IttdeXx8M=')_50%_/_cover_no-repeat] absolute left-[0px] top-[50%] translate-y-[-50%] w-[594.5px] h-[394px]">
+              </div>
+              <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/dd2513f7c5aa26c2189562147903579d3c3ec659?Expires=1711888365&KeyName=fn12-cdn&Signature=1l_qtCvePxACLv7nBVPpyQ0wvHk=')_50%_/_cover_no-repeat] absolute left-[103.2px] top-[63px] w-[388.5px] h-[386px]">
+              </div>
+              <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/a6f04d3d700f0ea5c6230efe3d3287089a1791f0?Expires=1711888365&KeyName=fn12-cdn&Signature=MCDSl57Hj5I_dKu9HP-Tc2zgqrg=')_50%_/_cover_no-repeat] absolute left-[124.3px] bottom-[0px] w-[347.2px] h-[460px]">
+              </div>
+              <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/9e891adfd116eb691f8b91b54fb6f6da1b93989a?Expires=1711888365&KeyName=fn12-cdn&Signature=Jaonnb_UCrOfpAHo47LoyHXRT-M=')_50%_/_cover_no-repeat] absolute left-[104.2px] top-[63px] w-[388px] h-[385.5px]">
+              </div>
+              <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/ceed8067dc33760174d3f67737f6e318d0f118ff?Expires=1711888365&KeyName=fn12-cdn&Signature=0F0H1MBI6DDHDv85AoRsnm-mSOM=')_50%_/_cover_no-repeat] absolute left-[104.2px] top-[63px] w-[387.5px] h-[385px]">
+              </div>
+            </div>
+          </div>
+          <div className="m-[0_0_16px_0] inline-block self-start break-words font-['Inter'] font-medium text-[17px] capitalize text-[#000000]">
+            Sacramento River Cats
+          </div>
+          <div className="rounded-[2px] bg-[#F7F7F8] relative flex flex-row justify-between p-[12px_0_4px_10.1px] w-[218.4px] box-sizing-border">
+            <div className="flex flex-col items-center box-sizing-border">
+              <div className="m-[0_0_6px_0] inline-block break-words font-['Inter'] font-normal text-[12px] capitalize text-[#525965]">
+                total events
+              </div>
+              <span className="m-[0_2.4px_0_0] break-words font-['Inter'] font-medium text-[14px] capitalize text-[#000000]">
+                48 Events
+              </span>
+            </div>
+            <div className="flex flex-col box-sizing-border">
+              <div className="m-[0_0_6px_0] inline-block self-start break-words font-['Inter'] font-normal text-[12px] capitalize text-[#525965]">
+                sport
+              </div>
+              <span className="break-words font-['Inter'] font-medium text-[14px] capitalize text-[#000000]">
+                baseball
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="shadow-[0px_4px_8px_0px_rgba(0,0,0,0.05)] bg-[#FFFFFF] relative flex flex-col p-[10px_10.1px_10px_10.1px] box-sizing-border">
+          <img className="m-[0_0_14.6px_0] w-[218.4px] h-[385.4px]" />
+          <div className="m-[0_0_16px_0] inline-block self-start break-words font-['Inter'] font-medium text-[17px] capitalize text-[#000000]">
+            Las Vegas Aviators
+          </div>
+          <div className="rounded-[2px] bg-[#F7F7F8] relative m-[0_0px_0_0] flex flex-row justify-between p-[12px_0_4px_10.1px] w-[218.4px] box-sizing-border">
+            <div className="flex flex-col items-center box-sizing-border">
+              <div className="m-[0_0_6px_0] inline-block break-words font-['Inter'] font-normal text-[12px] capitalize text-[#525965]">
+                total events
+              </div>
+              <span className="m-[0_3px_0_0] break-words font-['Inter'] font-medium text-[14px] capitalize text-[#000000]">
+                28 Events
+              </span>
+            </div>
+            <div className="flex flex-col box-sizing-border">
+              <div className="m-[0_0_6px_0] inline-block self-start break-words font-['Inter'] font-normal text-[12px] capitalize text-[#525965]">
+                sport
+              </div>
+              <span className="break-words font-['Inter'] font-medium text-[14px] capitalize text-[#000000]">
+                baseball
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="shadow-[0px_4px_8px_0px_rgba(0,0,0,0.05)] bg-[#FFFFFF] relative flex flex-col items-center p-[10px_10.1px_10px_10.1px] box-sizing-border">
+          <img className="m-[0_0_14.5px_0] w-[218.4px] h-[385.4px]" />
+          <div className="m-[0_1.4px_0_0] flex flex-col w-[fit-content] box-sizing-border">
+            <div className="m-[0_0_16.1px_0] inline-block self-start break-words font-['Inter'] font-medium text-[17px] capitalize text-[#000000]">
+              new jersey devils
+            </div>
+            <div className="rounded-[2px] bg-[#F7F7F8] relative flex flex-row justify-between p-[12px_0_4px_10px] w-[217px] box-sizing-border">
+              <div className="flex flex-col items-center box-sizing-border">
+                <div className="m-[0_0_6px_0] inline-block break-words font-['Inter'] font-normal text-[12px] capitalize text-[#525965]">
+                  total events
+                </div>
+                <span className="m-[0_5.1px_0_0] break-words font-['Inter'] font-medium text-[14px] capitalize text-[#000000]">
+                  15 Events
+                </span>
+              </div>
+              <div className="flex flex-col box-sizing-border">
+                <div className="m-[0_0_6px_0] inline-block self-start break-words font-['Inter'] font-normal text-[12px] capitalize text-[#525965]">
+                  sport
+                </div>
+                <span className="break-words font-['Inter'] font-medium text-[14px] capitalize text-[#000000]">
+                  ice hockey
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="shadow-[0px_4px_8px_0px_rgba(0,0,0,0.05)] bg-[#FFFFFF] relative flex flex-col p-[10px_10.1px_10px_10.1px] box-sizing-border">
+          <img className="m-[0_0_14.6px_0] w-[218.4px] h-[385.4px]" />
+          <div className="m-[0_0_16px_0] inline-block self-start break-words font-['Inter'] font-medium text-[17px] capitalize text-[#000000]">
+            Las Vegas Aviators
+          </div>
+          <div className="rounded-[2px] bg-[#F7F7F8] relative flex flex-row justify-between p-[12px_0_4px_10.1px] w-[218.4px] box-sizing-border">
+            <div className="flex flex-col items-center box-sizing-border">
+              <div className="m-[0_0_6px_0] inline-block break-words font-['Inter'] font-normal text-[12px] capitalize text-[#525965]">
+                total events
+              </div>
+              <span className="m-[0_3px_0_0] break-words font-['Inter'] font-medium text-[14px] capitalize text-[#000000]">
+                28 Events
+              </span>
+            </div>
+            <div className="flex flex-col box-sizing-border">
+              <div className="m-[0_0_6px_0] inline-block self-start break-words font-['Inter'] font-normal text-[12px] capitalize text-[#525965]">
+                sport
+              </div>
+              <span className="break-words font-['Inter'] font-medium text-[14px] capitalize text-[#000000]">
+                baseball
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="shadow-[0px_4px_8px_0px_rgba(0,0,0,0.05)] bg-[#FFFFFF] relative flex flex-col items-center p-[10px_10.1px_52px_10.1px] w-[238.5px] box-sizing-border">
+          <div className="border-[0.2px_solid_#006555] bg-[#FFFFFF] absolute left-[10.1px] top-[10px] w-[218.4px] h-[491px]">
+          </div>
+          <div className="bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-asia-southeast1/projects/SmiywNrDWtpeoyNUQYzATu/assets/images/c724731ef71497e0e3af85c77fa47046bb1f23cb?Expires=1711888365&KeyName=fn12-cdn&Signature=g8ru4obtJHKrbd3qUA4GJulTxDw=')_50%_/_cover_no-repeat] relative m-[0_0_28px_0] flex flex-row justify-end w-[fit-content] h-[218px] box-sizing-border">
+            <div className="bg-[#000000] flex flex-row justify-center p-[3px_15.9px_3px_15px] box-sizing-border">
+              <span className="break-words font-['Inter'] font-bold text-[12.8px] leading-[1.484] text-[#FFFFFF]">
+                Ad
+              </span>
+            </div>
+          </div>
+          <div className="relative m-[0_2px_8px_0] inline-block break-words font-['Inter'] font-semibold text-[20px] text-[#222D3A]">
+            Advertisement title
+          </div>
+          <span className="relative m-[0_4.7px_0_0] break-words font-['Inter'] font-normal text-[12.8px] leading-[1.484] text-[#525965]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </span>
+        </div>
+      </div>
+      <div className="shadow-[0px_4px_8px_0px_rgba(0,0,0,0.05)] rounded-[3px] bg-[#2C9CF0] m-[0_0_0_1px] flex flex-row justify-center self-center p-[10px_0.9px_10px_0] w-[124.8px] box-sizing-border">
+        <span className="break-words font-['Inter'] font-semibold text-[14px] leading-[1.849] text-[#FFFFFF]">
+          See More
+        </span>
+      </div>
+    </div>
+  )
+}
